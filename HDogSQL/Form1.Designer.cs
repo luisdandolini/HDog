26458bc8b314ad1a71df01009494d20abff29b6c
luisdandolini <82612638+luisdandolini@users.noreply.github.com>
2021-10-02 23:20:16 -0300

    Add files via upload

---
+++ HDogSQL/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace FormHotDogSQL
+{
+    partial class hotdog
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbDescricao = new System.Windows.Forms.TextBox();
+            this.tbValor = new System.Windows.Forms.TextBox();
+            this.tbNome = new System.Windows.Forms.TextBox();
+            this.btLimpar = new System.Windows.Forms.Button();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.dgHotdog = new System.Windows.Forms.DataGridView();
+            this.colID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDescricao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colValor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgHotdog)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Descrição:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(648, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Valor:";
+            // 
+            // tbDescricao
+            // 
+            this.tbDescricao.Location = new System.Drawing.Point(249, 32);
+            this.tbDescricao.Name = "tbDescricao";
+            this.tbDescricao.Size = new System.Drawing.Size(393, 20);
+            this.tbDescricao.TabIndex = 4;
+            // 
+            // tbValor
+            // 
+            this.tbValor.Location = new System.Drawing.Point(688, 32);
+            this.tbValor.Name = "tbValor";
+            this.tbValor.Size = new System.Drawing.Size(100, 20);
+            this.tbValor.TabIndex = 5;
+            // 
+            // tbNome
+            // 
+            this.tbNome.Location = new System.Drawing.Point(79, 32);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(100, 20);
+            this.tbNome.TabIndex = 6;
+            // 
+            // btLimpar
+            // 
+            this.btLimpar.Location = new System.Drawing.Point(632, 67);
+            this.btLimpar.Name = "btLimpar";
+            this.btLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btLimpar.TabIndex = 7;
+            this.btLimpar.Text = "Limpar";
+            this.btLimpar.UseVisualStyleBackColor = true;
+            this.btLimpar.Click += new System.EventHandler(this.btLimpar_Click);
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Location = new System.Drawing.Point(713, 67);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(75, 23);
+            this.btnInserir.TabIndex = 8;
+            this.btnInserir.Text = "Inserir";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // dgHotdog
+            // 
+            this.dgHotdog.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgHotdog.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colID,
+            this.colNome,
+            this.colDescricao,
+            this.colValor});
+            this.dgHotdog.Location = new System.Drawing.Point(12, 96);
+            this.dgHotdog.Name = "dgHotdog";
+            this.dgHotdog.Size = new System.Drawing.Size(776, 342);
+            this.dgHotdog.TabIndex = 9;
+            // 
+            // colID
+            // 
+            this.colID.HeaderText = "ID";
+            this.colID.Name = "colID";
+            this.colID.Width = 30;
+            // 
+            // colNome
+            // 
+            this.colNome.HeaderText = "Nome";
+            this.colNome.Name = "colNome";
+            this.colNome.Width = 115;
+            // 
+            // colDescricao
+            // 
+            this.colDescricao.FillWeight = 150F;
+            this.colDescricao.HeaderText = "Descrição";
+            this.colDescricao.Name = "colDescricao";
+            this.colDescricao.Width = 485;
+            // 
+            // colValor
+            // 
+            this.colValor.HeaderText = "Valor";
+            this.colValor.Name = "colValor";
+            // 
+            // hotdog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgHotdog);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.btLimpar);
+            this.Controls.Add(this.tbNome);
+            this.Controls.Add(this.tbValor);
+            this.Controls.Add(this.tbDescricao);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "hotdog";
+            this.Text = "Hot Dog";
+            this.Load += new System.EventHandler(this.hotdog_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgHotdog)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbDescricao;
+        private System.Windows.Forms.TextBox tbValor;
+        private System.Windows.Forms.TextBox tbNome;
+        private System.Windows.Forms.Button btLimpar;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.DataGridView dgHotdog;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDescricao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colValor;
+    }
+}
+
---
+++ HDogSQL/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace FormHotDogSQL
+{
+    public partial class hotdog : Form
+    {
+        public hotdog()
+        {
+            InitializeComponent();
+        }
+
+        private void hotdog_Load(object sender, EventArgs e)
+        {
+            atualizarGrid();
+        }
+
+        private MySqlConnectionStringBuilder conexaoBanco()
+        {
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "hotdog";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            conexaoBD.SslMode = 0;
+            return conexaoBD;
+        }
+
+        private void atualizarGrid()
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+                comandoMySql.CommandText = "SELECT * FROM hotdog WHERE ativoDog = 1";
+                MySqlDataReader reader = comandoMySql.ExecuteReader();
+
+                dgHotdog.Rows.Clear();
+
+                while (reader.Read())
+                {
+                    DataGridViewRow row = (DataGridViewRow)dgHotdog.Rows[0].Clone();
+                    row.Cells[0].Value = reader.GetString(0);//ID
+                    row.Cells[1].Value = reader.GetString(1);//NOME
+                    row.Cells[2].Value = reader.GetString(2);//DESCRIÇÃO
+                    row.Cells[3].Value = reader.GetString(3);//VALOR
+                    dgHotdog.Rows.Add(row);//ADICIONA LINHA NA TABELA
+                }
+                realizaConexacoBD.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection  ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void btLimpar_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+        }
+
+        private void limparCampos()
+        {
+            tbNome.Clear();
+            tbDescricao.Clear();
+            tbValor.Clear();
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+                comandoMySql.CommandText = "INSERT INTO hotdog (idDog,nomeDog,descricaoDog,valorDog) " +
+                    "VALUES('" + tbNome.Text + "', '" + tbDescricao.Text + "', " + tbValor.Text + ")";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close();
+                MessageBox.Show("Inserido com sucesso");
+                atualizarGrid();
+                limparCampos();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
